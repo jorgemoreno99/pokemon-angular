12d37e0e52d897e100b4f273b5850c06b7bbb43d
JorgeM <jorgemorenop99@gmail.com>
2025-04-02 14:32:08 +0200

    TESTS

--- src/app/components/views/details/details.component.spec.ts
+++ src/app/components/views/details/details.component.spec.ts
@@ -1,21 +1,118 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { DetailsComponent } from './details.component';
+import { of, Subject } from 'rxjs';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
-describe('DetailsComponent', () => {
+class PokemonServiceStub {
+  get(id: string) {
+    return of({
+      name: 'lucario',
+      height: 12,
+      weight: 540,
+      types: [
+        { type: { name: 'Fighting' } },
+        { type: { name: 'Steel' } }
+      ],
+      abilities: [
+        { ability: { name: 'Justified' } }
+      ],
+      sprites: {
+        front_default: '',
+        front_female: null,
+        front_shiny: ''
+      },
+      stats: [
+        { base_stat: 60, stat: { name: 'hp' } }
+      ]
+    });
+  }
+}
+
+class ActivatedRouteStub {
+  private subject = new Subject<any>();
+  paramMap = this.subject.asObservable();
+  setParamMap(params: { [key: string]: any }) {
+    this.subject.next({
+      get: (key: string) => params[key]
+    });
+  }
+}
+
+class RouterStub {
+  navigate(commands: any[]) { }
+}
+
+
+
+fdescribe('DetailsComponent', () => {
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
+  let pokemonService: PokemonService;
+  let router: Router;
+  let activatedRouteStub: ActivatedRouteStub;
 
   beforeEach(() => {
+    // CONFIGURATION TO USE ACTIVATED ROUTE PARAMS
+    activatedRouteStub = new ActivatedRouteStub();
     TestBed.configureTestingModule({
-      declarations: [DetailsComponent]
+      declarations: [DetailsComponent],
+      providers: [
+        { provide: PokemonService, useClass: PokemonServiceStub },
+        { provide: ActivatedRoute, useValue: activatedRouteStub },
+        { provide: Router, useClass: RouterStub }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     });
     fixture = TestBed.createComponent(DetailsComponent);
     component = fixture.componentInstance;
+    pokemonService = TestBed.inject(PokemonService);
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load pokemon info', fakeAsync(() => {
+    spyOn(component, 'loadInfo').and.callThrough();
+    activatedRouteStub.setParamMap({ id: '448' });
+    tick();
+    fixture.detectChanges();
+    expect(component.id).toBe('448');
+    expect(component.loadInfo).toHaveBeenCalled();
+    tick();
+    fixture.detectChanges();
+    expect(component.loaded).toBeTrue();
+    expect(component.pokemon).toBeDefined();
+    expect(component.pokemon.name).toEqual('lucario');
+  }));
+
+
+    it('backButtonHandler should navigate back home', () => {
+      const routerSpy = spyOn(router, 'navigate');
+      component.backButtonHandler();
+      expect(routerSpy).toHaveBeenCalledWith(['']);
+    });
+
+    it('movePokemon should move to next or prev pokemon and load info', fakeAsync(() => {
+      spyOn(component, 'loadInfo').and.callThrough();
+      component.id = '156';
+      component.movePokemon(1);
+      tick();
+      fixture.detectChanges();
+      expect(component.id).toEqual('157');
+      expect(component.loadInfo).toHaveBeenCalled();
+
+      component.id = '800';
+      component.movePokemon(-1);
+      fixture.detectChanges();
+      expect(component.id).toEqual('799');
+      expect(component.loadInfo).toHaveBeenCalledTimes(2);
+    }));
+
+
+
 });
--- src/app/components/views/home/home.component.spec.ts
+++ src/app/components/views/home/home.component.spec.ts
@@ -1,21 +1,131 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HomeComponent } from './home.component';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { of, throwError } from 'rxjs';
+
+class PokemonServiceStub {
+  getList(offset: number, limit: number) {
+    return of({
+      results: [
+        { name: 'bulbasaur' },
+        { name: 'ivysaur' },
+        { name: 'venusaur' }
+      ],
+      count: 1000
+    });
+  }
+}
+
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let pokemonService: PokemonService;
+  let debugElement: DebugElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [HomeComponent],
+      providers: [
+        { provide: PokemonService, useClass: PokemonServiceStub }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debugElement = fixture.debugElement;
+    pokemonService = TestBed.inject(PokemonService);
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call loadPokemonList and update vars', fakeAsync(() => {
+
+    spyOn(pokemonService, 'getList').and.callThrough();
+    component.ngOnInit();
+    tick();
+    fixture.detectChanges();
+
+    expect(pokemonService.getList).toHaveBeenCalledWith(component.offset, component.limit);
+    expect(component.pokemons.length).toBeGreaterThan(0);
+    expect(component.filteredPokemons.length).toBeGreaterThan(0);
+    expect(component.totalCount).toEqual(1000);
+
+    expect(component.limitOpions).toContain(1000);
+    expect(component.loaded).toBeTrue();
+  }));
+
+
+  describe('onSearch', () => {
+    beforeEach(fakeAsync(() => {
+      component.ngOnInit();
+      tick();
+      fixture.detectChanges();
+    }));
+
+
+    it('should filter pokemons by search input', () => {
+      component.onSearch('nusa'); // for venusaur
+      expect(component.filteredPokemons.length).toEqual(1);
+      expect(component.filteredPokemons[0].name).toEqual("venusaur");
+    });
+
+
+    it('should reset filteredPokemons when empty value', () => {
+      component.filteredPokemons = [{ name: 'charmander' , url: ""}];
+      component.onSearch('');
+      expect(component.filteredPokemons).toEqual(component.pokemons);
+    });
+  });
+
+
+  describe('moveOffset', () => {
+    beforeEach(fakeAsync(() => {
+      component.ngOnInit();
+      tick();
+      fixture.detectChanges();
+    }));
+
+    it('should increase offset and reload list ', fakeAsync(() => {
+      const initial = component.offset;
+      spyOn(component, 'loadPokemonList').and.callThrough();
+      component.moveOffset(1);
+      expect(component.offset).toEqual(initial + component.limit);
+      expect(component.loadPokemonList).toHaveBeenCalled();
+
+      component.offset = 400;
+      component.limit = 100;
+      component.moveOffset(-1);
+      expect(component.offset).toEqual(300);
+      expect(component.loadPokemonList).toHaveBeenCalledTimes(2);
+    }));
+
+
+
+    it('should adjust offset out of bounds', fakeAsync(() => {
+      component.offset = 20;
+      component.limit = 25; //initial
+      spyOn(component, 'loadPokemonList').and.callThrough();
+      component.moveOffset(-1);
+      expect(component.offset).toBe(0);
+      expect(component.loadPokemonList).toHaveBeenCalled();
+
+
+      component.offset = 980;
+      component.limit = 50;
+      component.moveOffset(-1);
+      expect(component.offset).toBe(930);
+      expect(component.loadPokemonList).toHaveBeenCalledTimes(2);
+
+    }));
+
+  });
+
+
 });
